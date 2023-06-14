f624e42e2afb5ee436417df6bdecc22bbb7cad51
BrianSantivanez <brian_santi95@hotmail.com>
2023-06-14 15:08:17 -0400

    added bootstrap, footer and header

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -10,14 +10,12 @@ import { PostComponent } from './post/post.component';
 export class FeedComponent {
   posts: Post[] = [
     {
-      value: `test1
-      \nLorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur placerat, dui sed malesuada fermentum, purus odio 
-      \nfaucibus nisi, ut laoreet nisi tortor ac augue. Pellentesque laoreet arcu non felis ullamcorper aliquet. Integer lobortis 
-      \ntincidunt ex. Maecenas pharetra ut felis vehicula accumsan. Quisque tincidunt urna sed vehicula euismod. Nullam id congue.`,
+      value: `test1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur placerat, dui sed malesuada fermentum, purus odio faucibus nisi, ut laoreet nisi tortor ac augue. Pellentesque laoreet arcu non felis ullamcorper aliquet. Integer lobortis tincidunt ex. Maecenas pharetra ut felis vehicula accumsan. Quisque tincidunt urna sed vehicula euismod. Nullam id congue.`,
       date: new Date()
     },
     {
-      value: 'test2',
+      value: `test2
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sem ex, ultrices quis euismod eget, dictum vitae nisl. Suspendisse congue diam eu aliquam convallis. Nulla mollis arcu vel lorem efficitur, nec euismod nulla cursus. Donec tincidunt elit ac elit hendrerit maximus. Proin pretium, risus ac fermentum placerat, ante nulla placerat ipsum, eu iaculis nisl diam quis nisi. Sed vulputate metus finibus justo cursus bibendum ut porttitor felis. Mauris justo massa, facilisis ut hendrerit non, euismod venenatis ipsum. Integer vel massa.`,
       date: new Date()
     },
     {
