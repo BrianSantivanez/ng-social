5da0e26c1aa1c735262e7c21147ec3fcd73fdb49
BrianSantivanez <brian_santi95@hotmail.com>
2023-06-24 18:47:25 -0400

    added about component and about link path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,11 @@ const routes: Routes = [
   {
     path: 'feed',
     loadChildren: () => import('./feed/feed.module').then((m) => m.FeedModule),
-  }
+  },
+  {
+    path : 'about',
+    component: AboutComponent
+  },
 ];
 
 @NgModule({
